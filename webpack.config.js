41149ac4918e9d57b81b94a55c3e440c6d522e63
Irina-lol <irinakazynkina@gmail.com>
2025-02-12 13:16:34 +0700

    Доработка

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: '/index.js',
+    output: {
+       filename: 'main.js'
+   },
+   plugins: [
+       new HtmlWebpackPlugin({template: '/index.pug'})
+   ],
+   module: {
+       rules: [
+               {
+               test: /\.pug$/,
+               loader: 'pug-loader',
+               options: {
+               pretty: true
+               }
+           }
+       ]
+   }
+};
+
